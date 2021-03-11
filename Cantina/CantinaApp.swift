fc989edd3a6b9f95c413f77a53177f6fc251cf66
Andrew Davis <drewdavis@hey.com>
2021-03-11 14:23:01 -0500

    Add Xcode project

---
+++ Cantina/CantinaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CantinaApp.swift
+//  Cantina
+//
+//  Created by Andrew Davis on 3/11/21.
+//
+
+import SwiftUI
+
+@main
+struct CantinaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cantina/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cantina
+//
+//  Created by Andrew Davis on 3/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
